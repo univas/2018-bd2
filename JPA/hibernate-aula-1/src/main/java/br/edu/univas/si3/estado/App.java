6fb0570eb98ca4a66e29cb7c794a83868cd29635
Roberto Rocha <rrocha.roberto@gmail.com>
2018-04-16 20:37:05 -0300

    Creating the maven project.

---
+++ JPA/hibernate-aula-1/src/main/java/br/edu/univas/si3/estado/App.java
@@ -0,0 +1,13 @@
+package br.edu.univas.si3.estado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
